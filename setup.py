55cb22ccddbe1b770a2602dbd67656a676a23e1d
Charles Holz <holzc@oregonstate.edu>
2025-08-08 21:29:34 -0700

    Migrated configuration options to pyproject.toml from setup.py.
    cmdclass scripts were not modified but all other information in the
    setup() function of setup.py was migrated.  Testing includes manual
    testing on Ubuntu with pipx only.
    
    Relates to #203.

--- setup.py
+++ setup.py
@@ -181,41 +181,41 @@ class UpdateI18n(Command):
 
 
 setup(
-    name=PACKAGE,
-    version=get_info("version"),
-    description=get_info("description"),
-    long_description=get_info("long_description"),
-    long_description_content_type="text/markdown",
-    author=get_info("author"),
-    maintainer=get_info("maintainer"),
-    maintainer_email=get_info("maintainer_email"),
-    url=get_info("url"),
-    license=get_info("license"),
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    include_package_data=True,
-    data_files=[
-        ("share/metainfo", ["data/io.github.GourmandRecipeManager.Gourmand.appdata.xml"]),
-        ("share/applications", ["data/io.github.GourmandRecipeManager.Gourmand.desktop"]),
-        ("share/icons/hicolor/scalable/apps", ["data/io.github.GourmandRecipeManager.Gourmand.svg"]),
-    ],
-    install_requires=[
-        "beautifulsoup4>=4.10.0",
-        'importlib-metadata; python_version<"3.10"',
-        "lxml>=4.6.3",
-        "pillow>=8.3.2",
-        "pygobject>=3.40.1,<=3.50.0",  # Issues with Ubuntu 22.04: https://github.com/beeware/toga/issues/3143
-        "sqlalchemy>=1.4.36,<2",
-        "tomli-w>=1.0.0",
-        'toml==0.10.2; python_version<"3.11"',
-        "recipe-scrapers>=14.27.0,<15",
-        "setuptools<81",
-    ],
-    extras_require={
-        "epub-export": ["ebooklib==0.17.1"],
-        "pdf-export": ["reportlab<4"],
-        "spellcheck": ["pyenchant", "pygtkspellcheck"],
-    },
+#    name=PACKAGE,
+#    version=get_info("version"),
+#    description=get_info("description"),
+#    long_description=get_info("long_description"),
+#    long_description_content_type="text/markdown",
+#    author=get_info("author"),
+#    maintainer=get_info("maintainer"),
+#    maintainer_email=get_info("maintainer_email"),
+#    url=get_info("url"),
+#    license=get_info("license"),
+#    package_dir={"": "src"},
+#    packages=find_packages("src"),
+#    include_package_data=True,
+#    data_files=[
+#        ("share/metainfo", ["data/io.github.GourmandRecipeManager.Gourmand.appdata.xml"]),
+#        ("share/applications", ["data/io.github.GourmandRecipeManager.Gourmand.desktop"]),
+#        ("share/icons/hicolor/scalable/apps", ["data/io.github.GourmandRecipeManager.Gourmand.svg"]),
+#    ],
+#    install_requires=[
+#        "beautifulsoup4>=4.10.0",
+#        'importlib-metadata; python_version<"3.10"',
+#        "lxml>=4.6.3",
+#        "pillow>=8.3.2",
+#        "pygobject>=3.40.1,<=3.50.0",  # Issues with Ubuntu 22.04: https://github.com/beeware/toga/issues/3143
+#        "sqlalchemy>=1.4.36,<2",
+#        "tomli-w>=1.0.0",
+#        'toml==0.10.2; python_version<"3.11"',
+#        "recipe-scrapers>=14.27.0,<15",
+#        "setuptools<81",
+#    ],
+#    extras_require={
+#        "epub-export": ["ebooklib==0.17.1"],
+#        "pdf-export": ["reportlab<4"],
+#        "spellcheck": ["pyenchant", "pygtkspellcheck"],
+#    },
     cmdclass={
         "bdist_wheel": BuildWheel,
         "build_i18n": BuildI18n,
@@ -223,9 +223,9 @@ setup(
         "sdist": BuildSource,
         "update_i18n": UpdateI18n,
     },
-    entry_points={
-        "gui_scripts": [
-            "gourmand = gourmand.main:launch_app",
-        ]
-    },
+#    entry_points={
+#        "gui_scripts": [
+#            "gourmand = gourmand.main:launch_app",
+#        ]
+#    },
 )
